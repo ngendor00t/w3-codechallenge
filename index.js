b52a24221987eb97b6013e81dce5e4bf10a2f3ba
Reginawambui <ngendoanita8@gmail.com>
2023-10-25 11:35:18 +0300

    completed assingment

--- index.js
+++ index.js
@@ -1,59 +1,32 @@
 
-document.addEventListener("DOMContentLoaded", function (){
-    getMovies()
+document.addEventListener("DOMContentLoaded", function () {
+    getMovies();
 });
-function getMovies(){
-        fetch("https://my-json-server.typicode.com/ngendor00t/w3-codechallenge/films")
 
-        .then (response =>response.json())
-        .then (data=> {
-            displayMovieDetails(data)
-            console.log(data)})
-        }
-//this function creates a list of all the films based on their title. 
-function displayMovieDetails(data){
-    for (let details of data){
+function getMovies() {
+    fetch("http://localhost:3000/films")
+        .then(response => response.json())
+        .then(data => {
+            displayMovieDetails(data);
+            console.log(data);
+        })
+        .catch(error => console.error("Error fetching movies:", error));
+}
+
+function displayMovieDetails(data) {
+    let list = document.getElementById("films");
+    for (let details of data) {
         console.log(details);
-        let list = document.getElementById("films");
         let li = document.createElement('li');
         li.textContent = details.title;
         li.addEventListener("click", () => displayFilmDetails(details));
         list.appendChild(li);
-
     }
 }
 
-// This function displays all movie details upon clicking the movie title.
-
-function displayFilmDetails(details){
-    let purchaseTickets = document.getElementById("buyTicket");
-    purchaseTicket.innerHTML =`
-    <ul id="films">
-      <img src="${details.poster}">
-      <p>${details.title}</p>
-      <p>Description: ${details.description}</p>
-      <p>Runtime: ${details.runtime}</p>
-      <p>Showtime: ${details.showtime}</p>
-      <p>Capacity: ${details.capacity}</p>
-      <p>Tickets_sold: ${details.tickets_sold}</p>
-      <button id="buyTicket">Buy Ticket</button>
-      <p id="soldOutMessage" style="color: blue;"></p>
-    </ul>
-    `;
-// the event listener updates the tickets counter and prints out "sold out" when the tickets are exhausted.
-    let buyTicketButton = document.getElementById("buyTicket");
-    buyTicketButton.addEventListener("click", () => {
-        if(details.tickets_sold < details.capacity){
-        return details.tickets_sold++;
-       }else if(details.tickets_sold === details.capacity){
-        soldOutMessage.textContent = "Sold out!";}
-    })
-};
-
-
-function displayFilmDetails(details){
+function displayFilmDetails(details) {
     let purchaseTickets = document.getElementById("purchaseTickets");
-    purchaseTickets.innerHTML =`
+    purchaseTickets.innerHTML = `
     <ul id="films">
       <img src="${details.poster}">
       <p>${details.title}</p>
@@ -66,13 +39,28 @@ function displayFilmDetails(details){
       <p id="soldOutMessage" style="color: brown;"></p>
     </ul>
     `;
-// the event listener updates the tickets counter and prints out "sold out" when the tickets are exhausted.
-    let buyTicketButton = document.getElementById("buyTicket");
-    buyTicketButton.addEventListener("click", () => {
-        if(details.tickets_sold < details.capacity){
-        return details.tickets_sold++;
-       }else if(details.tickets_sold === details.capacity){
-        soldOutMessage.textContent = "Sold out!";}
-    })
-};
 
+    let buyTicketButton = document.getElementById("buyTicket");
+    let soldOutMessage = document.getElementById("soldOutMessage");
+
+    buyTicketButton.addEventListener("click", () => {
+        if (details.tickets_sold < details.capacity) {
+            details.tickets_sold++;
+            purchaseTickets.innerHTML = `
+            <ul id="films">
+              <img src="${details.poster}">
+              <p>${details.title}</p>
+              <p>Description: ${details.description}</p>
+              <p>Runtime: ${details.runtime}</p>
+              <p>Showtime: ${details.showtime}</p>
+              <p>Capacity: ${details.capacity}</p>
+              <p>Tickets_sold: ${details.tickets_sold}</p>
+              <button id="buyTicket">Buy Ticket</button>
+              <p id="soldOutMessage" style="color: brown;"></p>
+            </ul>
+            `;
+        } else if (details.tickets_sold === details.capacity) {
+            soldOutMessage.textContent = "Sold out!";
+        }
+    });
+}
